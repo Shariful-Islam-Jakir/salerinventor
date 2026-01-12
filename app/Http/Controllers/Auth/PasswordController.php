2ed9e97a8e331288e1f398701f8a311247fb924a
Md Shariful Islam Jakir <jakirhabib88@gmail.com>
2026-01-12 14:53:54 +0600

    api is working

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Str;
+
 
 class PasswordController extends Controller
 {
@@ -16,8 +20,22 @@ class PasswordController extends Controller
             return response()->json([
                 'status'=> 'error',
                 'message'=> 'forget Password entry now'
-            ]);
+            ],Response::HTTP_NOT_FOUND);
          }
 
+         //generate random token
+
+         $token = Str::random(60);
+
+         //insert the token into password_reset_tokens
+
+         DB::table('password_reset_tokens')->updateOrInsert(
+            ['email'=> $email],
+            [
+                'email' =>$email,
+                'token' =>Hash::make($token),
+                'created_at' => Carbon::now()
+            ]
+         );
     }
 }
